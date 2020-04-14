baa7a1636b82f5cdb34611b5fcf6f9c5b85e1a34
judebrhanavan <60238991+judebrhanavan@users.noreply.github.com>
2020-04-14 02:04:44 +0100

    Timeseries forecast

---
+++ ForecastingHDD.R
@@ -0,0 +1,80 @@
+# Forecasting DailyHDD
+# Ref : https://otexts.com/fpp2/arima-r.html, https://otexts.com/fpp2/seasonal-arima.html
+
+setwd("C:/Users/AAJB/Desktop/MSc Data Analytics/Database and Analysis Programming/python/DAP_project")
+options(scipen=999)  # suppress scientic notation 
+
+# load necessary packages
+library(fpp2)
+library(forecast)
+library(ggplot2)
+
+# read csv file
+data <- read.csv('dailyhdd.csv')
+
+#create time series
+hdd_ts <- ts(data[,3], start = c(2009,311), frequency = 365.25)
+end(hdd_ts)
+# plot the timeseries 
+autoplot(hdd_ts) + xlab('Year') + ylab('Heating Energy Consumption')
+
+# trainng & test set
+trainingsetsize = length(hdd_ts) - 182 # test data would be last 6 months observations
+train_hdd <- subset(hdd_ts, end=trainingsetsize)
+test_hdd <- subset (hdd_ts, start = trainingsetsize+1)
+
+## fit ARIMA
+fit_arima = auto.arima(train_hdd)
+summary(fit_arima)
+fc_arima = forecast(fit_arima, h =182)
+checkresiduals(fc_arima)
+# residuals fail the white noise test 
+
+# but the point forecasts are good
+autoplot(fc_arima, PI=FALSE) + autolayer(test_hdd, series="Test Data") +xlab('Year') + ylab('Heating Energy Consumption')
+
+# -------------------------------------------------------------------------------------------------------------------
+# Foreasting hourlyHDD with dynamic regression 
+# Ref : https://otexts.com/fpp2/dynamic.html
+# Ref : https://otexts.com/fpp2/complexseasonality.html
+# Ref : https://robjhyndman.com/hyndsight/seasonal-periods/
+
+
+# read csv file
+data <- read.csv('combined.csv')
+
+hdd_ts <- msts(data[,3], start = c(2011,25), seasonal.periods=c(24,8766))
+temperature <- data[,2]
+
+# trainng set
+trainingsetsize = nrow(data) - 24*7
+train_hdd <- subset(hdd_ts, end=trainingsetsize)
+temp_train <- head(temperature, trainingsetsize)
+
+#test data 
+test_hdd <- subset (hdd_ts, start = trainingsetsize+1)
+temp_test <- tail(temperature, 24*7)
+
+
+## Harmonic regression : find optimal K values for daily and annual seasonality
+bestfit <- list(aicc=Inf)
+bestfourier <- numeric(2)
+
+for (i in 1:12){
+  for (j in 1:8){
+    myfourier <- c(i,j)
+    print(paste(paste("i is ",i), paste("j is ", j)))
+    z <- fourier(train_hdd, K=c(i,j))
+    fit <- auto.arima(train_hdd, xreg=cbind(z,temp_train), seasonal=FALSE)
+    if (fit$aicc < bestfit$aicc) {
+      bestfit <- fit
+      bestfourier <-myfourier
+    }
+  }
+}
+# bestfourier = 8 7, AICc=-14631.78 
+bestfourier
+fc <- forecast(bestfit, xreg=cbind(zf,temp_test), h=168)
+
+autoplot(fc)  + autolayer(test_hdd, series="Test Data") + xlab('Date') + ylab('Heating Energy Consumption')
+
